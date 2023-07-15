2a036869cbf677b7d7f1df907faa7edee7d5e17a
te94d <te94da@gmail.com>
2023-07-15 22:24:50 +0900

    DB設計

--- MumChaApp/models.py
+++ MumChaApp/models.py
@@ -6,9 +6,21 @@ class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
-    img = models.ImageField(null=True)
+    img = models.ImageField(upload_to="images", null=True)
     birth = models.DateField(null=True)
     comment = models.TextField(max_length=200, null=True)
+
+    #ユーザー名を表示する
+    def __str__(self):
+        return self.name
+
+    #フォロー人数を表示するfollow関数を定義
+    def follow_nums(self):
+        return len(Follow.objects.filter(owner=self.id))
+    
+    #フォロワー人数を表示するfollower_nums関数を定義
+    def follower_nums(self):
+        return len(Follow.objects.filter(follow_target=self.id))
 
 #投稿テーブル
 class Posting(models.Model):
@@ -20,8 +32,8 @@ class Posting(models.Model):
 #フォローテーブル
 class Follow(models.Model):
     follow_id = models.IntegerField(primary_key=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    target = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'do_follow_user')
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'accept_follow_user')
 
 #いいねテーブル
 class Favorite(models.Model):
